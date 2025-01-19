d0138526638a44c56da34349f0440d1a20912ed9
GrullonDev <prosystem155@gmail.com>
2025-01-19 08:06:35 -0600

    first commit

---
+++ lib/core/injection_container.dart
@@ -0,0 +1,7 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance; // sl = Service Locator
+
+Future<void> init() async {
+  // TODO: Registrar dependencias (repositorios, casos de uso, etc.)
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'core/injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('E-Commerce')),
+      body: const Center(
+        child: Text('¡Bienvenido a tu tienda en línea!'),
+      ),
+    );
+  }
+}
